988b23052330750174cff6dcf60cb8dc21ecb93e
Pingvu <nhlpool27@gmail.com>
2023-09-22 15:13:11 +0200

    Add project files.

---
+++ uppgift 2.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace uppgift
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv in första lönen på den anställda");
+            double anställd1= double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.Clear();
+
+            Console.WriteLine("Skirv in andra lönen på den anställda?");
+            double anställd2 =double.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Skriv in tredje lönen på den anställda");
+            double anställd3 = double.Parse(Console.ReadLine());
+
+            double medelvärde = (anställd1 + anställd2 + anställd3) / 3;
+
+            Console.WriteLine("Medelvärdet är " + medelvärde);
+
+            Console.ReadKey();
+        }
+    }
+}
